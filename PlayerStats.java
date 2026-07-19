fcfe58055d1e09d36b31ab2272f992158c85cdec
Daxz <Jhonnelubiano@gmail.com>
2026-07-19 11:01:59 +0800

    Chapter 3 complete - Added multiple classes and character creation

--- PlayerStats.java
+++ PlayerStats.java
@@ -9,31 +9,84 @@ public class PlayerStats {
         String playerName = input.nextLine();
     System.out.println("Welcome " + playerName + " !!!");
 
+    boolean characterCreated = false;
+
+    int health = 0;
+    int mana = 0;
+    int attack = 0;
+    int defense = 0;
+
+
+    String weapon = "";
+
+
     System.out.print("Enter your class: ");
         String playerClass = input.nextLine();
-            if (playerClass.equals("Warrior")) {
-    System.out.println("An amazing journey awaits you " + playerName + "!!!");
-    System.out.println("You received 1x basic sword");
+            if (playerClass.equalsIgnoreCase("Warrior")) {
+                characterCreated = true;
+                health = 200;
+                mana = 3;
+                attack = 10;
+                defense = 15;
+                weapon = "Basic Sword";
 
+    System.out.println("An amazing journey awaits you " + playerName);
             }
 
-           else if (playerClass.equals("Mage")) {
+            else if (playerClass.equalsIgnoreCase("Mage")) {
+                characterCreated = true;
+                health = 100;
+                mana = 20;
+                attack = 6;
+                defense = 5;
+                weapon = "Basic Grimoire";
+
     System.out.println("A wise mage you are " + playerName);
-    System.out.println("You received 1x basic staff");
-            
             }
 
-            else if (playerClass.equals("Archer")) {
-                System.out.println("A swift and fast archer you are " + playerName);
-                System.out.println("You received 1x basic bow");
-                System.out.println("You received 20x basic arrow");
+            else if (playerClass.equalsIgnoreCase("Archer")) {
+                characterCreated = true;
+                health = 100;
+                mana = 5;
+                attack = 10;
+                defense = 12;
+                weapon = "Basic Bow";
+
+    System.out.println("Breeze through the wind " + playerName);
                 
             }
 
             else {
-                System.out.println("Unknown class!");
+                health = 0;
+                mana = 0;
+                attack = 0;
+                defense = 0;
+                weapon = "N/A";
 
             }
+
+
+        if (characterCreated) {   
+System.out.println("Succesfully Created a Character!");
+    System.out.println("========== CHARACTER ==========");
+    System.out.println("Name: " + playerName);
+    System.out.println("Player Class: " + playerClass);
+System.out.println();
+    System.out.println("Health: " + health);
+    System.out.println("Mana: " + mana);
+    System.out.println("Attack: " + attack);
+    System.out.println("Defense: " + defense);
+    System.out.println("Weapon: " + weapon);
+    System.out.println("===============================");
+        }
+
+        else {
+            System.out.println();
+    System.out.println("Character Failed!");
+            System.out.println();
+System.out.println("Please try again.");
+
+        }
         
 
     input.close();
